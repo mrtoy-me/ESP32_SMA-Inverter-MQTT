bcb74955f604141dd9cb5f1545abfe0bee0938ab
Darryl Bond <darryl.bond@gmail.com>
2023-07-14 20:17:35 +1000

    Removed superfluous stuff

--- Config.h
+++ Config.h
@@ -8,14 +8,11 @@
 // 3=values and info and T+R+P-buffer
 #define DEBUG_SMA 1
 
-#define LOOPTIME_SEC 30
+
 
 // SMA login password for UG_USER or UG_INSTALLER always 12 char. Unused=0x00
 #define USERGROUP UG_USER
-/* char SmaInvPass[12]={'Z','a','q','1','2','w','s','x',0,0,0,0};  //Zaq12wsx
 
-// SMA blutooth address -> adapt to your system
-uint8_t SmaBTAddress[6]= {0x00, 0x80, 0x25, 0x27, 0x38, 0xE7}; // my SMA SMC6000TL 00:80:25:27:38:E7
 */
 // Configuration struct
 
